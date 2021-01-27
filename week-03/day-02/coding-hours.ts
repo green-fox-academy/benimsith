bb1793ff13703253ea79c9c7fab57c3c375c2e41
benimsith <56825755+benimsith@users.noreply.github.com>
2021-01-27 14:11:54 +0100

    Add files via upload
    array exercises

---
+++ week-03/day-02/coding-hours.ts
@@ -0,0 +1,16 @@
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+let dailyCoding = 6;
+let semester = 17;
+let hoursSpent = 17*5*6;
+let percent = hoursSpent/(17*52)*100;
+console.log(hoursSpent);
+console.log(percent);
+
